6476f63a7b9bdda476dcc2fdebb777b5464c649b
IvanonaMarina <linokstill@gmail.com>
2023-02-02 22:12:26 +0900

    исправила

--- R script рост девочек саха 0-3г Марина.R
+++ R script рост девочек саха 0-3г Марина.R
@@ -11,6 +11,7 @@ par(mgp = c(3, 1, 0))
 plot(lowess(Agemos, P5, f=.01), type="l", ylim=c(46, 103), xlim=c(0, 36), lty=1, lwd=1, col = "red", axes=F, xlab = "", ylab = "")
 title(main="", ylab="Length (cm)", xlab="Age (years)")
 
+
 # y axis left
 axis(side=2, at=seq(46, 103, 1), labels=FALSE, col.ticks="black")
 axis(side=2, at=seq(46, 103, 5), labels=seq(46, 103, 5), lwd.ticks=2)
@@ -24,7 +25,10 @@ axis(side=1, at=seq(0, 36, 1), labels=FALSE, col.ticks="black")
 axis(side=1, at=seq(0, 36, 12), labels=seq(0, 3, 1), lwd.ticks=2)
 
 lines(lowess(Agemos, P5, f=.01), lty=1, lwd=1, col = "red")
+
+
 lines(lowess(Agemos, P50, f=.01), lty=1, lwd=1, col = "red")
+
 lines(lowess(Agemos, P95, f=.01), lty=1, lwd=1, col = "red")
 
 text(max(Agemos)+0.5, max(P5),  "5th",  adj=0.3, cex=.55)
